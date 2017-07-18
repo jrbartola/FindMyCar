4e5402c1f4d6d958105e59788f7615f7a1c7f66a
Jesse Bartola <jrbartola@gmail.com>
2017-07-17 22:46:44 -0400

    Added foursquare API keys and .gitignore

--- RunningApp/MainViewController.swift
+++ RunningApp/MainViewController.swift
@@ -10,30 +10,61 @@ import UIKit
 import MapKit
 
 
-class MainViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MainViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet var searchBar: UISearchBar!
+    
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
-        self.mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        mapView.mapType = MKMapType.hybrid
         
-        self.mapView.mapType = MKMapType.hybrid
-        centerMapOnLocation()
+        centerMapOnLocation(coordinate: self.locationManager.location?.coordinate)
+        
+        searchBar.delegate = self
         
         
     }
     
-    func centerMapOnLocation() {
-        if let coordinate = self.locationManager.location?.coordinate {
+    func centerMapOnLocation(coordinate: CLLocationCoordinate2D?) {
+        if let coordinate = coordinate {
             self.mapView.setCenter(coordinate, animated: true)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.becomeFirstResponder()
+        print(searchBar.text!)
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(searchBar.text!) { (placemarks: [CLPlacemark]?, error: Error?) in
+            
+            if error == nil {
+                
+                let placemark = placemarks?.first
+                let anno = MKPointAnnotation()
+                anno.coordinate = (placemark?.location?.coordinate)!
+                anno.title = searchBar.text!
+                
+                let span = MKCoordinateSpanMake(0.075, 0.075)
+                let region = MKCoordinateRegion(center: anno.coordinate, span: span)
+                
+                self.mapView.setRegion(region, animated: true)
+                self.mapView.addAnnotation(anno)
+                self.mapView.selectAnnotation(anno, animated: true)
+                
+            } else {
+                print(error?.localizedDescription ?? "ERROR: ABORTING SEARCH")
+            }
         }
     }
 
